11b7d6c4f0ca7373c0d21b139035ed948708d575
João Mangueira <jamjs7@gmail.com>
2022-09-26 16:59:20 -0300

    removendo carro

--- js/main.js
+++ js/main.js
@@ -1,81 +1,110 @@
-(function () {
-    //isso serve para chamar diretamente igual é feito no jquery $('query selector')
-    const $ = querySelector => document.querySelector(querySelector);
+//isso serve para chamar diretamente igual é feito no jquery $('query selector')
+const $ = querySelector => document.querySelector(querySelector);
 
-    const getGarage = () => localStorage.garage
-        ? JSON.parse(localStorage.garage)
-        : [];
+const getGarage = () => localStorage.garage
+    ? JSON.parse(localStorage.garage)
+    : [];
 
 
-    const formatDate = time => {
-        return new Date(time).toLocaleTimeString('pt-br', {
-            hour: "numeric",
-            minute: "numeric"
-        })
-    };
+const formatDate = time => {
+    return new Date(time).toLocaleTimeString('pt-br', {
+        hour: "numeric",
+        minute: "numeric"
+    })
+};
 
-    const addCarToGarage = car => {
-        const tr = document.createElement("tr");
+const convertPeriod = millisecond => {
+    const min = Math.floor(millisecond / 60000);
+    const sec = Math.floor(millisecond % 60000 / 1000);
 
-        const tdModel = document.createElement("td");
-        tdModel.innerHTML = `${car.model}`;
-        tr.appendChild(tdModel);
+    return `${min}m e ${sec}s`;
+};
 
-        const tdLicense = document.createElement("td");
-        tdLicense.innerHTML = `${car.license}`
-        tr.appendChild(tdLicense);
+const checkout = license => {
+    const car = getGarage().filter(car => car.license === license);
+    let period = new Date() - new Date(car[0].time);
+    period = convertPeriod(period);
+    const msg = `O veículo ${car[0].model} de placa ${license} 
+    permanceu estacionado por ${period}. Deseja encerrar?`;
 
-        const tdTime = document.createElement("td");
-        tdTime.innerHTML = `${formatDate(car.time)}`
-        tr.appendChild(tdTime);
+    if(!confirm(msg)) {
+        return;
+    }
 
-        const tdFinish = document.createElement('td');
-        const btnFinish = document.createElement('button');
-        btnFinish.innerHTML = "X";
-        btnFinish.className = "btn btn-danger";
-        btnFinish.setAttribute("onclick", `removeCar('${car.plate}')`);
-        tdFinish.appendChild(btnFinish);
-        tr.appendChild(tdFinish);
+    removeCar(license);
+};
 
-        $("#result").appendChild(tr);
-    };
+const removeCar = license => {
+    const garage = getGarage().filter(car => car.license !== license);
 
-    const renderGarage = () => {
-        const garage = getGarage();
-        console.log(garage)
-        garage.forEach(addCarToGarage);
-    };
+    localStorage.garage = JSON.stringify(garage);
 
     renderGarage();
+};
 
-    $("#send")
-        .addEventListener("click", event => {
-            event.preventDefault();
+const addCarToGarage = car => {
+    const tr = document.createElement("tr");
 
-            const carModel = $("#car-model").value;
-            const license = $("#license-plate").value;
+    const tdModel = document.createElement("td");
+    tdModel.innerHTML = `${car.model}`;
+    tr.appendChild(tdModel);
 
-            if(!carModel || !license) {
-                $("#error").classList.toggle("hidden");
-                return;
-            }
+    const tdLicense = document.createElement("td");
+    tdLicense.innerHTML = `${car.license}`;
+    tr.appendChild(tdLicense);
 
-            $("#error").classList.add("hidden");
+    const tdTime = document.createElement("td");
+    tdTime.innerHTML = `${formatDate(car.time)}`;
+    tr.appendChild(tdTime);
 
-            const car = {
-                model: carModel,
-                license,
-                time: new Date()
-            };
+    const tdFinish = document.createElement('td');
+    const btnFinish = document.createElement('button');
+    btnFinish.innerHTML = "X";
+    btnFinish.className = "btn btn-danger delete";
+    btnFinish.setAttribute("onclick",  `checkout('${car.license}')`);
+    tdFinish.appendChild(btnFinish);
+    tr.appendChild(tdFinish);
 
-            const garage = getGarage();
+    $("#garage").appendChild(tr);
+};
 
-            garage.push(car);
-            localStorage.garage = JSON.stringify(garage);
+const renderGarage = () => {
+    const garage = getGarage();
+    $("#garage").innerHTML = "";
+    garage.forEach(addCarToGarage);
+};
 
-            addCarToGarage(car);
 
-            $("#car-model").value = "";
-            $("#license-plate").value = "";
-        });
-})();
+renderGarage();
+
+$("#send")
+    .addEventListener("click", event => {
+        event.preventDefault();
+
+        const carModel = $("#car-model").value;
+        const license = $("#license-plate").value;
+
+        if(!carModel || !license) {
+            $("#error").classList.toggle("hidden");
+            return;
+        }
+
+        $("#error").classList.add("hidden");
+
+        const car = {
+            model: carModel,
+            license,
+            time: new Date()
+        };
+
+        const garage = getGarage();
+
+        garage.push(car);
+        localStorage.garage = JSON.stringify(garage);
+
+        addCarToGarage(car);
+
+        $("#car-model").value = "";
+        $("#license-plate").value = "";
+    });
+
